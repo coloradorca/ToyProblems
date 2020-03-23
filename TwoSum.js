6b2a7dbf8b2bf08e58a542052278dc8dd9273bfd
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-03-23 10:19:19 -0600

    solutions to TwoSum

--- TwoSum.js
+++ TwoSum.js
@@ -9,4 +9,30 @@
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
 
-var twoSum = function(nums, target) {};
+//First Solution =>  Time Complexity is O(n^2) && Space Complexity is O(1)
+
+// var twoSum = function(nums, target) {
+//   for (var k = 1; k < nums.length; k++) {
+//     for (var i = 0; i < nums.length; i++) {
+//       if (nums[i] + nums[k] === target) {
+//         return [i, k];
+//       }
+//     }
+//   }
+// };
+// console.log(twoSum([2, 7, 11, 15], 13));
+
+//Second Solution with O(n)
+
+function twoSum(nums, target) {
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    const number = target - nums[i];
+    if (number in hash) {
+      return [hash[number], i];
+    }
+    hash[nums[i]] = i;
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 17));
