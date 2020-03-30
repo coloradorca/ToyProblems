a31c215e129ca5b310cab944193aff2780940017
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-03-30 10:22:33 -0600

    Solve defangIPaddr and update Readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ToyProblems
 
 Daily Toy Problems to keep fresh with Algorithms and JS
+
+## Running Tests
+
+As of now, I'm completing these Problems on my local IDE and running the tests in the browser.
+
+## Where are these problems coming from
+
+LeetCode
+
+Codewars
--- defangIPaddr.js
+++ defangIPaddr.js
@@ -1,3 +1,5 @@
+const { performance } = require('perf_hooks');
+
 // Given a valid (IPv4) IP address, return a defanged version of that IP address.
 
 // A defanged IP address replaces every period "." with "[.]".
@@ -11,6 +13,40 @@
 // Input: address = "255.100.50.0"
 // Output: "255[.]100[.]50[.]0"
 
-var defangIPaddr = function(address) {};
+//  In an attempt to see how the performance of the native .split() and .join() compare against the
+//  standard for loop, I'm incorporating the performance.now() feature.  The results I'm see when
+//  running the different functions on my local machine are too inconsistent to make conclusions
+//  about which one is faster.  It seems as though its neglible at such a small scale.
+
+var t0 = performance.now();
+
+var defangIPaddr = function(address) {
+  var result = '';
+  for (var i = 0; i < address.length; i++) {
+    if (address[i] === '.') {
+      result += '[';
+      result += '.';
+      result += ']';
+    } else {
+      result += address[i];
+    }
+  }
+  return result;
+};
+// var defangIPaddr = function(address) {
+//   let str = '';
+//   for (let i = 0; i < address.length; i++) {
+//     str += address[i] !== '.' ? address[i] : '[.]';
+//   }
+//   return str;
+// };
+
+// var defangIPaddr = function(address) {
+//   return address.split('.').join('[.]');
+// };
+
+var t1 = performance.now();
+
+// console.log('Performance of given function is' + (t1 - t0) + ' milliseconds.');
 
 console.log(defangIPaddr('1.1.1.1'));
