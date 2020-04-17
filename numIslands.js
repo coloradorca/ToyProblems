8964c3ad726552ded77f191a5a69295fd409f448
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-04-17 13:37:11 -0600

    solve num islands

--- numIslands.js
+++ numIslands.js
@@ -22,8 +22,56 @@ Input:
 00011
 
 Output: 3
+
+
+Graph Algorithm- (depth first search)
+
+strategy:
+
+use recursion check each place on the grid for left, right, down, above neighbors, and 'sink' each island when we reach it
+
+
 */
 
-var numIslands = function (grid) {};
+var numIslands = function (grid) {
+  if (!grid || grid.length === 0) return 0;
+  let islands = 0;
 
-console.log(numIslands());
+  for (var i = 0; i < grid.length; i++) {
+    for (var k = 0; k < grid[i].length; k++) {
+      if (grid[i][k] === '1') {
+        islands += dfs(grid, i, k);
+      }
+    }
+  }
+
+  let dfs = (grid, i, k) => {
+    //out of bounds of matrix or current index is 0 (stop traversing)
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      k < 0 ||
+      k >= grid[i].length ||
+      grid[i][k] === '0'
+    ) {
+      return 0;
+    }
+    //sink the island
+    grid[i][k] = '0';
+    //down index
+    dfs(grid, i + 1, k);
+    // up index
+    dfs(grid, i - 1, k);
+    //right neighbor
+    dfs(grid, i, k + 1);
+    //left neighbor
+    dfs(grid, i, k - 1);
+    //account for original seen island
+    return 1;
+  };
+};
+
+console.log(numIslands(11110
+  11010
+  11000
+  00000));
