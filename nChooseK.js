4c617009768ec7a2d887d11ada453bbd7d474612
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-09-24 11:38:36 -0600

    finish n choose K, binomial coefficient

---
+++ nChooseK.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/55b22ef242ad87345c0000b2
+
+// You may be familiar with the concept of combinations: for example, if you take 5 cards from a 52 cards deck as you would playing poker, you can have a certain number (2,598,960, would you say?) of different combinations.
+
+// In mathematics the number of k combinations you can have taking from a set of n elements is called the binomial coefficient of n and k, more popularly called n choose k.
+
+// The formula to compute it is relatively simple: n choose k==n!/(k!*(n-k)!), where ! of course denotes the factorial operator.
+
+// You are now to create a choose function that computes the binomial coefficient, like this:
+
+// choose(1,1)==1
+// choose(5,4)==5
+// choose(10,5)==252
+// choose(10,20)==0
+// choose(52,5)==2598960
+// Be warned: a certain degree of optimization is expected, both to deal with larger numbers precision (and their rounding errors in languages like JS) and computing time.
+
+// const choose = (n,k) => {
+
+//   const factorialize =(num) => {
+//     var result = num;
+//     if (num === 0 || num === 1)
+//     return 1;
+//     while (num > 1) {
+//       num--;
+//       result *= num;
+//     }
+//     return result
+//   }
+//   return Math.round((factorialize(n)) / (factorialize(k) * factorialize(n-k)))
+// }
+
+
+//recursive solution
+function choose(n,k){
+    return (k === 0)? 1 : choose(n - 1,k - 1) * n/k ;
+}
+
+
+  console.log(choose(500,4))
